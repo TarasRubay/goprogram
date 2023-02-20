2484438fc5a3e3fed2d7aab18376bdc63c49e0ef
RubayTaras <rubaytaras@gmail.com>
2023-02-20 12:49:49 +0200

    count rectangel

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/goprogram
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type point struct {
+	x, y int
+}
+
+func countRectangles(points []point) int {
+	count := 0
+	for i := 0; i < len(points); i++ {
+		for j := i + 1; j < len(points); j++ {
+			for k := j + 1; k < len(points); k++ {
+				if points[i].x == points[j].x && points[k].x == points[k].x && points[i].y == points[k].y && points[j].y == points[k].y {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	n := 10
+	points := make([]point, n)
+	for i := 0; i < n; i++ {
+		points[i] = point{rand.Intn(10), rand.Intn(10)}
+	}
+	fmt.Println("Points:", points)
+	fmt.Println("Rectangles:", countRectangles(points))
+}
